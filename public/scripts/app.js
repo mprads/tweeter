94a613071f93a0b87eb2b2b369973b01f0e5968d
Myles Rademaker <mylesprademaker@gmail.com>
2017-01-24 21:00:58 +0000

    Completed hover function on tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,4 +3,10 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
+$(document).ready(() => {
+  $("#tweet-container").on("mouseenter", "article", (event) => {
+    $(event.currentTarget).find("header").css("opacity", "1");
+  }).on("mouseleave", "article", (event) => {
+    $(event.currentTarget).find("header").css("opacity", ".5");
+  });
+});
