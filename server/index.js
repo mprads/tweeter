76f7aa8a6908e5bd71376ba3ea2d96e5f38eb1bd
Myles Rademaker <mylesprademaker@gmail.com>
2017-01-27 18:46:38 +0000

    fixed mobile display

--- server/index.js
+++ server/index.js
@@ -8,14 +8,12 @@ const bodyParser    = require("body-parser");
 const app           = express();
 const MongoClient = require("mongodb").MongoClient;
 require('dotenv').config();
-// const MONGODB_URI = "heroku config:set MONGODB_URI=mongodb://ds133279.mlab.com:33279/tweeter";
 const MONGODB_URI = process.env.MONGODB_URI;
 const makeDataHelpers = require("./lib/data-helpers.js");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// The in-memory database of tweets. It's a basic object with an array in it.
 const db = MongoClient.connect(MONGODB_URI, (err, db) => {
   if (err) {
     console.error(`Failed to connect: ${MONGODB_URI}`);
@@ -33,15 +31,3 @@ const db = MongoClient.connect(MONGODB_URI, (err, db) => {
   });
 
 });
-// The `data-helpers` module provides an interface to the database of tweets.
-
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-//
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-
-// Mount the tweets routes at the "/tweets" path prefix:
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,8 +1,5 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
-
-// Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
     saveTweet: function(newTweet, callback) {
@@ -10,7 +7,6 @@ module.exports = function makeDataHelpers(db) {
       callback(null, true);
     },
 
-    // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
       db.collection("tweets").find().toArray((err, tweets) => {
         if (err) {
